6a80123a810c801fd4f420ef2cf572ae7869f96a
TJ Holowaychuk <tj@vision-media.ca>
2009-03-10 15:55:58 -0700

    Revised using new Mini

---
+++ assets/helpers.rb
@@ -0,0 +1,14 @@
+
+def example file
+  File.read File.dirname(__FILE__) + '/examples/' + file
+end
+
+def title title, subheading
+  %(<h1>#{title} <em>#{subheading}</em></h1>)
+end
+
+def project_url username, project
+  "http://github.com/#{username}/#{project}"
+end
+
+PROJECT_URL = project_url('visionmedia', 'jspec')
