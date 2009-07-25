bb5c5abe11a8c1edd7dad5569e4cf2c2896961c1
visionmedia <tj@vision-media.ca>
2009-07-25 15:53:22 -0700

    Fixed html encoding

--- assets/helpers.rb
+++ assets/helpers.rb
@@ -1,13 +1,16 @@
 
 require 'rubygems'
+require 'rext/all'
 require 'lightr'
 
 def highlight string
   Lightr::JavaScript.parse(string).to_s
 end
 
-def example file
-  File.read File.dirname(__FILE__) + '/examples/' + file
+def example file, escape = false
+  contents = File.read File.dirname(__FILE__) + '/examples/' + file
+  contents = contents.escape_html
+  contents
 end
 
 def title title, subheading
