8e89c005a249edea600d4978660eef95a37bf076
visionmedia <tj@vision-media.ca>
2009-10-12 12:34:40 -0700

    Removed JSpec.paramsFor()

--- spec/spec.utils.js
+++ spec/spec.utils.js
@@ -259,21 +259,4 @@ describe 'Utility'
     end
   end
   
-  describe 'paramsFor()'
-    it 'should return an array of function parameter names'
-      JSpec.paramsFor(function(foo, bar){}).should.eql ['foo', 'bar']
-    end
-    
-    it 'should return only the params for the root function'
-      foo = function(bar){
-        function baz(test) {}
-        var something = function(foo, bar){}
-      }
-      JSpec.paramsFor(foo).should.eql ['bar']
-    end
-    
-    it 'should return empty array when no params are present'
-      JSpec.paramsFor(function(){}).should.eql []
-    end
-  end
 end
