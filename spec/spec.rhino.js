50e55d4ed0dbcc5af8ecaac08897cff90d884750
visionmedia <tj@vision-media.ca>
2009-10-16 14:12:02 -0700

    Fixed Env.js support

--- spec/spec.rhino.js
+++ spec/spec.rhino.js
@@ -1,7 +1,10 @@
 
 load('spec/env.js')
+Envjs('spec/fixtures/test.html')
+load('spec/jquery.js')
 load('lib/jspec.js')
 load('lib/jspec.xhr.js')
+load('lib/jspec.jquery.js')
 load('spec/helpers.js')
 load('spec/spec.grammar-less.js')
 
@@ -10,9 +13,12 @@ JSpec
 .exec('spec/spec.js')
 .exec('spec/spec.matchers.js')
 .exec('spec/spec.utils.js')
+.exec('spec/spec.jquery.js')
 .exec('spec/spec.fixtures.js')
 .exec('spec/spec.shared-behaviors.js')
 .exec('spec/spec.modules.js')
 .exec('spec/spec.xhr.js')
-.run({ formatter : JSpec.formatters.Terminal, failuresOnly : false })
+.exec('spec/spec.jquery.xhr.js')
+.run({ formatter: JSpec.formatters.Terminal, failuresOnly: false })
 .report()
+
