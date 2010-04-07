a1d81da64786666c4c43184d48e0cbbcb67b6f6f
Pieter Noordhuis <pcnoordhuis@gmail.com>
2010-04-07 11:20:38 +0200

    don't append trailing whitespace to error messages

--- spec/unit/spec.js
+++ spec/unit/spec.js
@@ -134,8 +134,8 @@ describe 'Failing specs'
       false.should.be_true
       true.should.be_false
     })
-    spec.should.have_failure_message('expected false to be true ')
-    spec.should.have_failure_message('expected true to be false ')
+    spec.should.have_failure_message('expected false to be true')
+    spec.should.have_failure_message('expected true to be false')
   end
   
   it 'should catch exceptions throw within specs'
