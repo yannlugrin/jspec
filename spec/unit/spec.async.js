9f8320aab77b95f7b335f19a18b79ae64c2b5f1b
visionmedia <tj@vision-media.ca>
2010-04-08 11:05:43 -0700

    Moved failing timeout spec up to see effects on other sepcs

--- spec/unit/spec.async.js
+++ spec/unit/spec.async.js
@@ -12,6 +12,19 @@ describe 'Asynchronous specs'
       end
       
       describe 'given a timeout'
+        it 'should fail when passing the timeout'
+          expecting(2, { timeout: 200 })
+          setTimeout(-{
+            true.should.be_true
+          }, 100)
+          setTimeout(-{
+            true.should.be_true
+          }, 300)
+          setTimeout(-{
+            true.should.be_true
+          }, 400)
+        end
+        
         it 'should pass when within the timeout'
           expecting(2, { timeout: 400 })
           setTimeout(-{
@@ -20,19 +33,6 @@ describe 'Asynchronous specs'
           setTimeout(-{
             true.should.be_true
           }, 200)
-        end
-        
-        it 'should fail when passing the timeout'
-          // expecting(2, { timeout: 200 })
-          // setTimeout(-{
-          //   true.should.be_true
-          // }, 100)
-          // setTimeout(-{
-          //   true.should.be_true
-          // }, 300)
-          // setTimeout(-{
-          //   true.should.be_true
-          // }, 400)
         end
       end
     end
